0599aaac887ac366a4e2fe49335bc3770a4d04c2
VuXuanThanh-Dev <devxuanthanhvu0503@gmail.com>
2021-06-05 14:01:54 +0700

    cheap

---
+++ script.js
@@ -0,0 +1,18 @@
+const cheap = document.querySelectorAll('.cheap');
+const  balls = document.querySelectorAll('.ball');
+const checks = document.querySelectorAll('input[type="checkbox"]');
+
+
+
+
+// balls.forEach((ball, idx) => {
+    
+//     ball.addEventListener('click', () =>{
+//         let check = checks[idx];
+//         if(check.checked == true) {
+//             cheap[idx].classList.add('active');
+//         }else {
+//             cheap[idx].classList.remove('active');
+//         }
+//     });
+// });
